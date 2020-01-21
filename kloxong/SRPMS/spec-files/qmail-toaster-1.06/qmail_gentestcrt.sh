7e1a54dfdf7f52d6eb79e4d59c411aedacd60ed5
dkstiler <dkstiler@gmail.com>
2020-01-22 00:02:06 +0200

    Update qmail_gentestcrt.sh

--- kloxong/SRPMS/spec-files/qmail-toaster-1.06/qmail_gentestcrt.sh
+++ kloxong/SRPMS/spec-files/qmail-toaster-1.06/qmail_gentestcrt.sh
@@ -127,7 +127,7 @@ EOT
     echo "______________________________________________________________________"
     echo ""
     echo "${T_MD}RESULT:${T_ME}"
-    $openssl verify ca.crt
+    $openssl verify -untrusted ca.crt
     if [ $? -ne 0 ]; then
         echo "cca:Error: Failed to verify resulting X.509 certificate" 1>&2
         exit 1
