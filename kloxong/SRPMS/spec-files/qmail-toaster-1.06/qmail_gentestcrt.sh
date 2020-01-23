529b825731f7786f7a6a453e442b6a1a8de51f00
dkstiler <dkstiler@gmail.com>
2020-01-23 08:03:39 +0200

    fix centos 8 server generated certificate
    Trick centos 8 th think that the certificate is valid by checking against itself

--- kloxong/SRPMS/spec-files/qmail-toaster-1.06/qmail_gentestcrt.sh
+++ kloxong/SRPMS/spec-files/qmail-toaster-1.06/qmail_gentestcrt.sh
@@ -211,7 +211,8 @@ EOT
     echo "______________________________________________________________________"
     echo ""
     echo "${T_MD}RESULT:${T_ME}"
-    $openssl verify -CAfile ca.crt -CAfile $user.crt $user.crt
+ #   $openssl verify -CAfile ca.crt -CAfile $user.crt $user.crt
+     $openssl verify -CAfile $user.crt $user.crt
     if [ $? -ne 0 ]; then
         echo "cca:Error: Failed to verify resulting X.509 certificate" 1>&2
         exit 1
