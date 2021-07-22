9cdfef3c5447bf85380cd0c57cf31bdb83bd78ba
John Pierce <john@luckytanuki.com>
2021-07-22 19:14:37 +1000

    Relocating program folders to their own Repositories

--- kloxong/SRPMS/spec-files/kloxong-webmail-afterlogic/afterlogic_inc_settings_path.php
+++
@@ -1,6 +0,0 @@
-<?php
-	// MR -- if want move to outsite web access change 'data' to '/home/kloxo/httpd/afterlogic/data'
-	// and then move '/home/kloxo/httpd/webmail/afterlogic/data' to '/home/kloxo/httpd/afterlogic/data'
-
-	$dataPath = 'data';
-?>
